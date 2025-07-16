e246d8f1f4a51ac5f0272b444596b85a3aa54578
SaadTech360 <saadibrahimumar7@gmail.com>
2025-07-16 22:01:04 +0100

    fix: update the memberregiserform phone and graduation_year fields

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,22 @@
 from django.contrib.auth.forms   import UserCreationForm,UserChangeForm
 from django.contrib.auth.models  import User
 from .models  import ShisMemberUser
-
+from django    import forms
 class MembersRegisterForm(UserCreationForm):
 
     class Meta:
         model = ShisMemberUser
         fields = ['first_name','last_name','nick_name','photo','email','phone','graduation_year']
+        widgets = {
+            'graduation_year': forms.DateInput(attrs={'type':'date'}),
+            'phone': forms.NumberInput(attrs={'type':'tel'}),
+        }
 
 class MembersChangeform(UserChangeForm):
     class Meta:
         model = ShisMemberUser
         fields = ['first_name','last_name','nick_name','photo','email','phone','graduation_year','status']
+        widgets = {
+            'graduation_year': forms.DateInput(attrs={'type':'date'}),
+            'phone': forms.NumberInput(attrs={'type':'tel'}),
+        }
